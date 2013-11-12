e659dc5685f8552b4ab5a8e4536e3bd63aa200fd
Michal Malik <root@devrandom.sk>
2013-11-12 22:33:41 +0100

    added some comments for better readability

--- disasm.c
+++ disasm.c
@@ -16,22 +16,30 @@ static const char *regs = "ABCDXYZJ";
 
 static void dis_opr(u16 *mem, u16 b, u16 ip, char *out) {
 	u16 w = 0;
+
 	if(b >= 0x10 && b <= 0x19) {
 		w = mem[ip+1];
 	}
 
+	// register
 	if(b >= 0x00 && b <= 0x07) {
 		sprintf(out+strlen(out), "%c", regs[b]);
+	// [register]
 	} else if(b >= 0x08 && b <= 0x0F) {
 		sprintf(out+strlen(out), "[%c]", regs[b-0x08]);
+	// [register+nextw]
 	} else if(b >= 0x10 && b <= 0x17) {
 		sprintf(out+strlen(out), "[%c+0x%04X]", regs[b-0x10], w);
+	// nextw
 	} else if(b == 0x18) {
 		sprintf(out+strlen(out), "0x%04X", w);
+	// [nextw]
 	} else if(b == 0x19) {
 		sprintf(out+strlen(out), "[0x%04X]", w);
+	// SP
 	} else if(b == 0x1A) {
 		sprintf(out+strlen(out), "SP");
+	// IP
 	} else if(b == 0x1B) {
 		sprintf(out+strlen(out), "IP");
 	}
@@ -52,12 +60,16 @@ void disassemble(u16 *mem, u16 ip, char *out) {
 		sprintf(out+strlen(out), "%s ", opc[op]);
 	}
 
+	// NOT, JMP, JTR, PUSH, POP, IAR, INT
 	if((op == 6) || (op >= 0x12 && op <= 0x15) || (op >= 0x18 && op <= 0x19)) {
 		dis_opr(mem, ins>>11, ip, out);
+	// RET, RETI
 	} else if(op == 0x16 || op == 0x17) {
 
+	// DAT
 	} else if(op > ins_num) {
 
+	// everything else
 	} else {
 		dis_opr(mem, ins>>11, ip, out);
 		sprintf(out+strlen(out), ",");
