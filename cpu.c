657567435bc1e649c27d9365e6e43ebfe7891b76
Michal Malik <root@devrandom.sk>
2013-11-29 20:53:27 +0100

    update cpu
    - added support for hardware devices
    - refactored the code
    - fixed the multiple IFE - IFLNE instruction bug

--- cpu.c
+++ cpu.c
@@ -4,13 +4,16 @@
 #include <stdarg.h>
 #include <time.h>
 
-#define zero(a)     (memset((a),0,sizeof((a))))
-#define count(a)    (sizeof((a))/sizeof((a)[0]))
+#define zero(a)         (memset((a),0,sizeof((a))))
+#define count(a)        (sizeof((a))/sizeof((a)[0]))
 
-// 1 Mhz
-// nanoseconds
+#define OPO(op)         (op&0x3f)
+#define OPD(op)         (op>>11)
+#define OPS(op)         ((op>>6)&0x1f)
+#define USINGNW(w)      ((w >= 0x10 && w <= 0x19))
+
+// 1 Mhz in nanoseconds
 #define CPU_TICK                1000           
-
 #define STACK_START             0x8000
 #define STACK_LIMIT             0x2000
 
@@ -19,12 +22,48 @@ typedef unsigned short u16;
 
 extern void disassemble(u16 *mem, u16 ip, char *out);
 
+// Load init functions for devices
+extern void floppy_init();
+extern void floppy_handle_interrupt(u16 hwi);
+
+extern void monitor_init();
+extern void monitor_handle_interrupt(u16 hwi);
+
+enum _registers {
+        rA, rB, rC, rD, rX, rY, rZ, rJ
+};
+
+enum _instructions {
+        STO,
+        ADD, SUB, MUL, DIV, MOD,
+        NOT, AND, OR, XOR, SHL, SHR,
+        IFE, IFN, IFG, IFL, IFGE, IFLE,
+        JMP, JTR, PUSH, POP, RET, RETI,
+        IAR, INT, HWI, HWQ, HWN
+};
+
+// union is not needed, we don't 
+// need to access the mid and hid separately
+struct device {
+        struct device *next;
+
+        u16 index; // hardware index
+        u16 mid; // manufacter id
+        u16 hid; // hardware id
+
+        void (*_init)();
+        void (*_handle_interrupt)(u16);
+};
+
 struct cpu {
         u16 r[8];
         u16 sp, ip, ia;
         u8 of:1;
         u16 mem[0x8000];
-        int cycles;
+        unsigned int cycles;
+
+        // Linked list of devices
+        struct device *devices;
 };
 
 void error(const char *format, ...) {
@@ -40,6 +79,68 @@ void error(const char *format, ...) {
 
         puts(buf_1);
         exit(1);
+}
+
+// id contains mid and hid
+// _init is the init function for the device
+// _handle_interrupt is the routine that handles all hardware interrupts
+static unsigned int add_device(struct cpu *p, unsigned int id, void (*_init)(), void (*_handle_interrupt)(u16)){
+        struct device *dev = NULL;
+        u16 mid = 0, hid = 0;
+
+        mid = (id>>16)&0xffff;
+        hid = id&0xffff;
+
+        for(dev = p->devices; dev; dev = dev->next) {
+                if(mid == dev->mid && hid == dev->hid) {
+                        return 0;
+                }
+        }
+
+        dev = (struct device *)malloc(sizeof(struct device));
+        memset(dev, 0, sizeof(struct device));
+
+        if(!p->devices) dev->index = 0;
+        else dev->index = p->devices->index+1;
+        dev->mid = mid;
+        dev->hid = hid;
+        dev->_init = _init;
+        dev->_handle_interrupt = _handle_interrupt;
+
+        dev->next = p->devices;
+        p->devices = dev;
+        return 1;
+}
+
+static struct device *find_device(struct cpu *p, u16 index) {
+        struct device *dev = NULL;
+        for(dev = p->devices; dev; dev = dev->next) {
+                if(index == dev->index) {
+                        return dev;
+                }
+        }
+        return NULL;
+}
+
+static u16 count_devices(struct cpu *p) {
+        struct device *dev = p->devices;
+        u16 ret = 0;
+        while(dev) {
+                ret++;
+                dev = dev->next;
+        }
+        return ret;
+}
+
+static void free_devices(struct cpu *p) {
+        if(!p->devices) return;
+        struct device *head = p->devices, *tmp = NULL;
+        while(head) {
+                tmp = head;
+                head = head->next;
+                free(tmp);
+        }
+        free(head);
 }
 
 static void memory_dmp(struct cpu *p, const char *fn) {
@@ -59,14 +160,16 @@ static void memory_dmp(struct cpu *p, const char *fn) {
 }
 
 static void debug(struct cpu *p) {
-        printf("%2s: 0x%04X\n", "A", p->r[0]);
-        printf("%2s: 0x%04X\n", "B", p->r[1]);
-        printf("%2s: 0x%04X\n", "C", p->r[2]);
-        printf("%2s: 0x%04X\n", "D", p->r[3]);
-        printf("%2s: 0x%04X\n", "X", p->r[4]);
-        printf("%2s: 0x%04X\n", "Y", p->r[5]);
-        printf("%2s: 0x%04X\n", "Z", p->r[6]);
-        printf("%2s: 0x%04X\n", "J", p->r[7]);
+        size_t i;
+
+        printf("%2s: 0x%04X\n", "A", p->r[rA]);
+        printf("%2s: 0x%04X\n", "B", p->r[rB]);
+        printf("%2s: 0x%04X\n", "C", p->r[rC]);
+        printf("%2s: 0x%04X\n", "D", p->r[rD]);
+        printf("%2s: 0x%04X\n", "X", p->r[rX]);
+        printf("%2s: 0x%04X\n", "Y", p->r[rY]);
+        printf("%2s: 0x%04X\n", "Z", p->r[rZ]);
+        printf("%2s: 0x%04X\n", "J", p->r[rJ]);
         printf("%2s: 0x%04X\n", "IA", p->ia);
         printf("%2s: 0x%04X\n", "SP", p->sp);
         printf("%2s: 0x%04X\n", "IP", p->ip);
@@ -75,7 +178,6 @@ static void debug(struct cpu *p) {
         printf("\n");
 
         printf("Stack:\n");
-        size_t i;
         for(i = 0; i < 0x40; i++) {
                 printf("%04X ", p->mem[(STACK_START-0x40)+i]);
                 if((i+1)%16==0) printf("\n");
@@ -87,7 +189,7 @@ static void load(struct cpu *p, const char *fn) {
         u8 b1 = 0, b2 = 0;
         u16 i = 0;
 
-        if(!(fp = fopen(fn, "rb"))) {
+        if((fp = fopen(fn, "rb")) == NULL) {
                 error("file \"%s\" not found or not readable", fn);
         }
 
@@ -101,7 +203,7 @@ static void load(struct cpu *p, const char *fn) {
 static u16 *getopr(struct cpu *p, u8 b, u16 *w) {
         u16 *o = NULL;
 
-        if(b >= 0x10 && b <= 0x19) {
+        if(USINGNW(b)) {
                 w = p->mem+(p->ip++);
                 p->cycles++;
         }
@@ -143,118 +245,124 @@ static u16 *getopr(struct cpu *p, u8 b, u16 *w) {
         return o;
 }
 
-static int word(u16 op) {
-        u8 wc = 1;
-        u16 o = op&0x3f;
-        u8 d = op>>11;
-        u8 s = (op>>6)&0x1f;
+static int jump(struct cpu *p) {
+        u8 wc = 0;
+        u16 o, oldo;
+        u8 d, s;
+        u16 op = *(p->mem+p->ip);
 
-        if(d >= 0x10 && d <= 0x19) wc++;
-        if(s >= 0x10 && s <= 0x19) wc++;
+        do {
+                wc++;
+                o = OPO(op);
+                d = OPD(op);
+                s = OPS(op);
+
+                oldo = o;
+
+                if(USINGNW(d)) wc++;
+                if(USINGNW(s)) wc++;
+                op = *(p->mem+p->ip+wc);
+        } while(oldo >= IFE && oldo <= IFLE);
 
         return wc;
 }
 
 static void step(struct cpu *p) {
         char out[128] = {0};
+        u16 op = 0;
+        u8 o = 0;
+        u16 *d = NULL, *s = NULL, w = 0;
+        u16 wc = 0;
+        unsigned int start_cyc = p->cycles;
+        struct timespec slp = {0};
+
         disassemble(p->mem, p->ip, out);
         printf("%s", out);
 
-        int start_cyc = p->cycles;
-        struct timespec slp = {0, 0};
-
         p->cycles++;
 
-        u16 op = p->mem[p->ip++];
-
-        u8 o = 0;
-        u16 *d = 0, *s = 0, w = 0;
-        u16 wc = 0;
-
-        o = op&0x3f;
-        d = getopr(p, op>>11, &w);
-        s = getopr(p, (op>>6)&0x1f, &w);
-
-        wc = word(*(p->mem+p->ip));
+        op = p->mem[p->ip++];
+        o = OPO(op);
+        d = getopr(p, OPD(op), &w);
+        s = getopr(p, OPS(op), &w);
+        wc = jump(p);
 
         switch(o) {
-                // SET
-                case 0: *d = *s; break;
+                // STO
+                case STO: *d = *s; break;
                 // ADD
-                case 1: {
+                case ADD: {
                         if(*s+*d>0xFFFF) p->of = 1; 
                         *d = (*s+*d)&0xffff; 
-                        break;
-                }
+                } break;
                 // SUB
-                case 2: *d = (*d-*s)&0xffff; break;
+                case SUB: *d = (*d-*s)&0xffff; break;
                 // MUL
-                case 3: *d = (*s*(*d))&0xffff; break;
+                case MUL: *d = (*s*(*d))&0xffff; break;
                 // DIV
-                case 4: {
+                case DIV: {
                         *d = (*d/(*s));
-                        p->r[3] = (*d)%(*s);
+                        p->r[ rD ] = (*d)%(*s);
                         p->cycles++;
                 } break;
                 // MOD
-                case 5: {
+                case MOD: {
                         *d = (*d)%(*s);
                 } break;
                 // NOT
-                case 6: {
+                case NOT: {
                         *d = ~(*d);
-                        break;
-                }
+                } break;
                 // AND
-                case 7: {
+                case AND: {
                         *d = (*d)&(*s);
                 } break;
                 // OR
-                case 8: {
+                case OR: {
                         *d = (*d)|(*s);
                 } break;
                 // XOR
-                case 9: {
+                case XOR: {
                         *d = (*d)^(*s);
                 } break;
                 // SHL
-                case 0x0A: {
+                case SHL: {
                         *d = ((*d)<<(*s))&0xffff;
                 } break;
                 // SHR
-                case 0x0B: {
+                case SHR: {
                         *d = ((*d)>>(*s))&0xffff;
                 } break;
                 // IFE
-                case 0x0C: {
+                case IFE: {
                         if(*d != *s) p->ip += wc;
                 } break;
                 // IFN
-                case 0x0D: {
+                case IFN: {
                         if(*d == *s) p->ip += wc;
                 } break;
                 // IFG
-                case 0x0E: {
+                case IFG: {
                         if(*d <= *s) p->ip += wc;
                 } break;
                 // IFL
-                case 0x0F: {
+                case IFL: {
                         if(*d >= *s) p->ip += wc;
                 } break;
                 // IFGE
-                case 0x10: {
+                case IFGE: {
                         if(*d < *s) p->ip += wc; 
                 } break;
                 // IFLE
-                case 0x11: {
+                case IFLE: {
                         if(*d > *s) p->ip += wc;
                 }
                 // JMP
-                case 0x12: {
+                case JMP: {
                         p->ip = *d;
                 } break;
                 // JTR
-                case 0x13: {
+                case JTR: {
                         // push IP on stack
                         p->mem[--p->sp] = p->ip;
                         p->cycles++;
@@ -262,20 +370,20 @@ static void step(struct cpu *p) {
                         p->ip = *d; 
                 } break;
                 // PUSH
-                case 0x14: {
+                case PUSH: {
                         p->mem[--p->sp] = *d;
                 } break;
                 // POP
-                case 0x15: {
+                case POP: {
                         *d = p->mem[p->sp++];
                 } break;
                 // RET
-                case 0x16: {
+                case RET: {
                         // pop IP from stack
                         p->ip = p->mem[p->sp++];
                 } break;
                 // RETI
-                case 0x17: {
+                case RETI: {
                         // pop IP from stack
                         p->ip = p->mem[p->sp++];
                         // set IA to 0, means we are returning
@@ -283,7 +391,7 @@ static void step(struct cpu *p) {
                         p->ia = 0;
                 } break;
                 // IAR 
-                case 0x18: {
+                case IAR: {
                         // set IA to a value
                         // means we are expecting an interrupt routine
                         // to be entered
@@ -291,13 +399,39 @@ static void step(struct cpu *p) {
 
                 } break;
                 // INT
-                case 0x19: {
+                case INT: {
                         // set A to the message
-                        p->r[0] = *d;
+                        p->r[ rA ] = *d;
                         // push return IP to stack
                         p->mem[--p->sp] = p->ip;
                         // jump to IA
                         p->ip = p->ia;
+                } break;
+                // HWI
+                case HWI: {
+                        // Find the device by its hardware index
+                        // If it doesn't exist, we are done
+                        struct device *dev = find_device(p, *d);
+                        if(!dev) break;
+
+                        // Call the handle interrupt method
+                        // of the device
+                        dev->_handle_interrupt(p->ia);
+                } break;
+                // HWQ
+                case HWQ: {
+                          struct device *dev = find_device(p, *d);
+                          if(dev) {
+                                p->r[rA] = dev->mid;
+                                p->r[rB] = dev->hid;
+                          } else {
+                                p->r[rA] = 0;
+                                p->r[rB] = 0;
+                          }
+                } break;
+                // HWN
+                case HWN: {
+                        *d = count_devices(p);
                 } break;
                 default: break;
         }
@@ -312,25 +446,37 @@ static void step(struct cpu *p) {
 int main(int argc, char **argv) {
         char i_fn[128] = {0};
         char o_fn[128] = {0};
+        static struct cpu p = {0};
 
         if(argc < 3) {
                 error("usage: %s <program> <memory_dump>", argv[0]);
         }
-
         strcpy(i_fn, argv[1]);
         strcpy(o_fn, argv[2]);
 
-        static struct cpu p;
-        zero(&p);
-        zero(p.mem);
-
+        // Init cpu and its devices
         load(&p, i_fn);
+        
+        // Add floppy
+        // MID: 0x32BA
+        // HID: 0x236E
+        // _init = floppy_init()
+        // _handle_interrupt = floppy_handle_interrupt(u16)
 
-        p.cycles = 0;
+        // TODO: floppy_init() will set MID and HID etc.
+        add_device(&p, 0x32ba236e, floppy_init, floppy_handle_interrupt);
+
+        // Add monitor
+        // MID: 0xFF21
+        // HID: 0xBEBA
+        // _init = monitor_init()
+        // _handle_interrupt = monitor_handle_interrupt()
+        add_device(&p, 0xff21beba, monitor_init, monitor_handle_interrupt);
+
         p.sp = STACK_START;
 
         int run = 0, c = 0;
-        int w1 = 0, w2 = 0;
+        unsigned int w1 = 0, w2 = 0;
         do {
                 if(p.sp < STACK_START-STACK_LIMIT-1) {
                         error("STACK_LIMIT(%04X) reached", STACK_LIMIT);
@@ -359,5 +505,6 @@ int main(int argc, char **argv) {
 
         memory_dmp(&p, o_fn);
 
+        free_devices(&p);
         return 0;
 }
