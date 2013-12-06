83f668b04cd2e682f1daf9e3c4a7a31fd1c9ed24
Michal Malik <root@devrandom.sk>
2013-12-07 00:24:27 +0100

    update: the DEVICE_ID is now static const

--- floppy.c
+++ floppy.c
@@ -4,18 +4,17 @@
 
 #include "cpu.h"
 
-#define DEVICE_ID	0x32ba236e
-
+static const u32 DEVICE_ID = 0x32ba236e;
 static struct cpu *proc;
 
 u32 floppy_init(struct cpu *p) {
 	proc = p;
 
-	printf("Initiating floppy, id 0x%08X\n", DEVICE_ID);
+	printf("Initiating floppy, id 0x%08x\n", DEVICE_ID);
 
 	return DEVICE_ID;
 }
 
 void floppy_handle_interrupt(u16 hwi) {
-	printf("Floppy received an interrupt: 0x%04X\n", hwi);
+	printf("Floppy received an interrupt: 0x%04x\n", hwi);
 }
--- monitor.c
+++ monitor.c
@@ -4,18 +4,17 @@
 
 #include "cpu.h"
 
-#define DEVICE_ID	0xff21beba
-
+static const u32 DEVICE_ID = 0xff21beba;
 static struct cpu *proc;
 
 u32 monitor_init(struct cpu *p) {
 	proc = p;
 
-	printf("Initiating monitor, id 0x%08X\n", DEVICE_ID);
+	printf("Initiating monitor, id 0x%08x\n", DEVICE_ID);
 
 	return DEVICE_ID;
 }
 
 void monitor_handle_interrupt(u16 hwi) {
-	printf("Monitor received an interrupt: 0x%04X\n", hwi);
+	printf("Monitor received an interrupt: 0x%04x\n", hwi);
 }
